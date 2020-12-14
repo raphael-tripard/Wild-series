4c21d36a629f27c42b4afea42aa6ed0ce85de28e
Raphaël Tripard <tripard.raphael@gmail.com>
2020-12-14 11:11:43 +0100

    06 - Symfony : Créer ta première entité avec Doctrine

---
+++ src/Entity/Category.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CategoryRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CategoryRepository::class)
+ */
+class Category
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Program;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getProgram(): ?string
+    {
+        return $this->Program;
+    }
+
+    public function setProgram(string $Program): self
+    {
+        $this->Program = $Program;
+
+        return $this;
+    }
+}
